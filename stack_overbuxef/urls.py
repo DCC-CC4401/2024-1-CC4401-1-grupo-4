635d5426146173b1c7dbf4d7b241efaab1540731
Andrés Calderón Guardia <andres.calderon@ug.uchile.cl>
2024-04-25 17:14:31 -0400

    start project

---
+++ stack_overbuxef/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path('register', views.register_user, name='register_user'),
+    # path('login',views.login_user, name='login'),
+    # path('logout',views.logout_user, name='logout'),
+    # path('forum',views.logout_user, name='forum'),
+    # path('message',views.logout_user, name='message'),
+]
