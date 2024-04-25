48010574495cedaa81dc767e59c95260c6bad01a
Andrés Calderón Guardia <andres.calderon@ug.uchile.cl>
2024-04-25 17:45:15 -0400

    login and register basic template

--- stack_overbuxef/urls.py
+++ stack_overbuxef/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('register', views.register_user, name='register_user'),
-    # path('login',views.login_user, name='login'),
+    path('', views.login_user, name='login'),
+    path('register', views.register_user, name='register_user'),
     # path('logout',views.logout_user, name='logout'),
     # path('forum',views.logout_user, name='forum'),
     # path('message',views.logout_user, name='message'),
--- stack_overbuxef/views.py
+++ stack_overbuxef/views.py
@@ -4,9 +4,9 @@ from django.shortcuts import render, redirect
 # Create your views here.
 from django.http import HttpResponseRedirect
 
-# def register_user(request):
-#     if request.method == 'GET': #Si estamos cargando la página
-#         return render(request, "todoapp/register_user.html") #Mostrar el template
+def register_user(request):
+    if request.method == 'GET': #Si estamos cargando la página
+        return render(request, "stack_overbuxef/register_user.html") #Mostrar el template
 
 #     elif request.method == 'POST': #Si estamos recibiendo el form de registro
 
@@ -23,21 +23,16 @@ from django.http import HttpResponseRedirect
 #         #Redireccionar la página /tareas
 #         return HttpResponseRedirect('/tareas')
 
-# from django.contrib.auth import authenticate, login,logout
-# def login_user(request):
-#     if request.method == 'GET':
-#         return render(request,"todoapp/login.html")
-#     if request.method == 'POST':
-#         username = request.POST['username']
-#         contraseña = request.POST['contraseña']
-#         usuario = authenticate(username=username,password=contraseña)
-#         if usuario is not None:
-#             login(request,usuario)
-#             return HttpResponseRedirect('/tareas')
-#         else:
-#             return HttpResponseRedirect('/register')
-
-
-# def logout_user(request):
-#     logout(request)
-#     return HttpResponseRedirect('/tareas')
+from django.contrib.auth import authenticate, login
+def login_user(request):
+    if request.method == 'GET':
+        return render(request, "stack_overbuxef/login.html")
+    if request.method == 'POST':
+        username = request.POST['username']
+        contraseña = request.POST['contraseña']
+        usuario = authenticate(username=username, password=contraseña)
+        if usuario is not None:
+            login(request, usuario)
+            return HttpResponseRedirect('/forum')
+        else:
+            return HttpResponseRedirect('/register')
