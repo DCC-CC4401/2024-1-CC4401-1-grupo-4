b3292ae9299190539404bd9ead481e84debf4629
frank3421 <franco.navarron@gmail.com>
2024-07-15 16:48:46 -0400

    Cambios para merge

--- stack_overbuxef/urls.py
+++ stack_overbuxef/urls.py
@@ -22,8 +22,8 @@ urlpatterns = [
     path('delete_tag/<int:tag_id>/', views.deleteTag, name='delete_tag'),
     path('select2/', include("django_select2.urls")),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-    path('like/<int:answer_id>/', views.like_answer, name='like_answer'),
-    path('dislike/<int:answer_id>/', views.dislike_answer, name='dislike_answer')
+    path('like1/<int:answer_id>/', views.like_answer, name='like_answer'),
+    path('dislike1/<int:answer_id>/', views.dislike_answer, name='dislike_answer')
 ]
 
 if settings.DEBUG:
